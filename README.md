2fa1d20fb0bcce52bc5b91eab00190381dc2f220
Dean Petty <dean.petty@gmail.com>
2018-10-05 22:27:06 -0500

    Added vidoes list has begun to be altered. Added images folder.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # HomeFlix
 An at home video streaming webserver built with Node.js and Express. Giving you full control of your media!
 
-## How to Setup
-Install your basic node/express instance. Will have more detailed instructions in the future. 
-
 ## PATH_TO_VIDEOS
 This project heavily relies on the environmental variable PATH_TO_VIDEOS. Setting this to the directoy where your video files are is required for HomeFlix to find your media to be able to serve them!
+
+## How to Setup
+You will need npm and Node in order to get this project to run.
+
+1 ) git clone https://github.com/criticalfault/HomeFlix.git
+2 ) cd Homeflix
+3 ) npm install
+4 ) PATH_TO_VIDEOS={{Your full path here}} node bin/www
+5 ) open your web browser localhost:3000
--- routes/index.js
+++ routes/index.js
@@ -6,43 +6,54 @@ const PATH = process.env.PATH_TO_VIDEOS;
 let file_list = fs.readdirSync(PATH);
 let videos_list = [];
 
-//file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false && (mime.lookup(PATH+"/"+video) == "video/mp4" || mime.lookup(PATH+"/"+video) == "video/x-matroska" || mime.lookup(PATH+"/"+video) == "video/x-msvideo") );
-file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false && mime.lookup(PATH+"/"+video) == "video/mp4");
-for(var i=0; i < file_list.length; i++)
-{
-	//Trying to decide how best to return an array with path, filename, etc. Object may be it!
-	videos_list.push({path: file_list[i], name: file_list[i].split('/').pop(), mime:mime.lookup(PATH+"/"+file_list[i]) });
+function findVideos(){
+	//file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false && (mime.lookup(PATH+"/"+video) == "video/mp4" || mime.lookup(PATH+"/"+video) == "video/x-matroska" || mime.lookup(PATH+"/"+video) == "video/x-msvideo") );
+	file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false && mime.lookup(PATH+"/"+video) == "video/mp4");
+	for(var i=0; i < file_list.length; i++)
+	{
+		//Trying to decide how best to return an array with path, filename, etc. Object may be it!
+		videos_list.push({path: file_list[i], name: file_list[i].split('/').pop().split('.').unshift(), mime:mime.lookup(PATH+"/"+file_list[i]) });
+	}
 }
 
+findVideos();
 
 //Mongo would be ideal for storing information about videos!
 //const mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Homeflix' });
+  res.render('index', { title: 'HomeFlix' });
 });
 
-router.get('/video-list', function(req, res, next)
-{
-	res.render('video-list', { "list": videos_list, title: "Master Video List" });
+router.get('/config', function(req, res, next) {
+  res.render('config', { title: 'HomeFlix' });
 });
 
+router.post('/config', function(req, res, next) {
 
-router.get('/video-play/:id', function(req,res,next)
-{
-	res.render('video-player', { id:req.params.id });
+	//Handle all of the Post Requests for the config module here
+
+  res.render('config', { title: 'HomeFlix' });
 });
 
-router.get('/serve-video/:id', function(req, res, next)
-{
+router.get('/video-list', function(req, res, next){
+	res.render('video-list', { title: "Master Video List", "list": videos_list });
+});
+
+router.get('/video-play/:id', function(req,res,next){
+	res.render('video-player', { title: 'HomeFlix', id: req.params.id });
+});
+
+router.get('/serve-video/:id', function(req, res, next){
+
 	const path = PATH+"/"+videos_list[req.params.id].path;
 	const stat = fs.statSync(path)
 	const fileSize = stat.size;
 	const range = req.headers.range;
 	
-	if (range) 
-	{
+	if (range) {
+
 		const parts = range.replace(/bytes=/, "").split("-")
 		const start = parseInt(parts[0], 10)
 		const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1
@@ -59,9 +70,8 @@ router.get('/serve-video/:id', function(req, res, next)
 
 		res.writeHead(206, head);
 		file.pipe(res);
-	}
-	else 
-	{
+
+	}else{
 
 		const head = 
 		{
