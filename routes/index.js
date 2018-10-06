00a7c9da7a82e0d89d72c1c0b75d52104f8e21ae
Dean Petty <dean.petty@gmail.com>
2018-10-05 21:24:07 -0500

    Added a master files building list instead of doing multiple calls of the same files

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-
+const mime = require('mime-types')
 const PATH = process.env.PATH_TO_VIDEOS;
+let file_list = fs.readdirSync(PATH);
+let videos_list = [];
+
+//file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false && (mime.lookup(PATH+"/"+video) == "video/mp4" || mime.lookup(PATH+"/"+video) == "video/x-matroska" || mime.lookup(PATH+"/"+video) == "video/x-msvideo") );
+file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false && mime.lookup(PATH+"/"+video) == "video/mp4");
+for(var i=0; i < file_list.length; i++)
+{
+	//Trying to decide how best to return an array with path, filename, etc. Object may be it!
+	videos_list.push({path: file_list[i], name: file_list[i].split('/').pop(), mime:mime.lookup(PATH+"/"+file_list[i]) });
+}
+
 
 //Mongo would be ideal for storing information about videos!
 //const mongoose = require('mongoose');
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Homeflix' });
 });
 
-
 router.get('/video-list', function(req, res, next)
 {
-	let file_list = fs.readdirSync(PATH);
-
-	file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false);
-	
-	let videos_list = [];
-
-	for(var i=0; i < file_list.length; i++)
-	{
-		//Trying to decide how best to return an array with path, filename, etc. Object may be it!
-		videos_list.push({path: file_list[i], name: file_list[i].split('/').pop() });
-	}
-
-
 	res.render('video-list', { "list": videos_list, title: "Master Video List" });
 });
 
 
 router.get('/video-play/:id', function(req,res,next)
 {
-
-	res.render('video-player', {id:req.params.id})
-
+	res.render('video-player', { id:req.params.id });
 });
 
 router.get('/serve-video/:id', function(req, res, next)
 {
-
-	//use req.params.id to find stuff in our DB or against our JSON object...
-
-
-	// This will need the logic 
-	let file_list = fs.readdirSync(PATH);
-
-	file_list = file_list.filter(video => fs.lstatSync(PATH+"/"+video).isFile() == true && fs.lstatSync(PATH+"/"+video).isDirectory() == false);
-	
-	let videos_list = [];
-
-	for(var i=0; i < file_list.length; i++)
-	{
-		//Trying to decide how best to return an array with path, filename, etc. Object may be it!
-		videos_list.push({path: file_list[i], name: file_list[i].split('/').pop() });
-	}
-
-
-
 	const path = PATH+"/"+videos_list[req.params.id].path;
-	console.log(videos_list);
-	console.log(path);
 	const stat = fs.statSync(path)
 	const fileSize = stat.size;
 	const range = req.headers.range;
@@ -83,9 +56,13 @@ router.get('/serve-video/:id', function(req, res, next)
 			'Content-Length': chunksize,
 			'Content-Type': 'video/mp4',
 		}
+
 		res.writeHead(206, head);
 		file.pipe(res);
-	} else {
+	}
+	else 
+	{
+
 		const head = 
 		{
 			'Content-Length': fileSize,
@@ -95,6 +72,5 @@ router.get('/serve-video/:id', function(req, res, next)
 		fs.createReadStream(path).pipe(res)
 	}
 });
-
 
 module.exports = router;
