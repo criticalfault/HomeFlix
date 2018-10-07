f06eab11bd0e6cd590ae0435301c637aed63177a
Emily <ekaneff@accusoft.com>
2018-10-06 23:16:32 -0400

    removed console log and temp module description

--- modules/config/main.js
+++ modules/config/main.js
@@ -1,13 +1,8 @@
-// this module is just going to read in the config file and export the object so that it can be required 
-// anywhere in the project and we dont have to worry about env variables
-
 const fs = require('fs')
 const path = require('path')
 const configFile = "config.json"
 
 let rawData = fs.readFileSync(path.resolve(configFile));
 let parsedData = JSON.parse(rawData);
-
-//console.log(parsedData);
 
 module.exports = parsedData
