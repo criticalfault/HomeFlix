be8730f15d067babc462d6386f34bc39fb81590d
bretw84 <bretw84@gmail.com>
2018-10-26 17:52:42 -0500

    Fixed Back To Top arrow - wasnt working, and made hero image a background image so it fills scrren for responsive screens

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,23 +1,27 @@
-$('#backTop').click(function(){
-    $('#mainNav').removeClass('fixHeader');
-    $('html').animate({scrollTop: 5}, 500);
-});
+$(document).ready(function() {
 
-var fixed = false;
-$(window).scroll(function() {
-    if(window.scrollY > 220) {
-        $('#mainNav').addClass('fixHeader');
-        $('#mainBody').addClass('fixBody');
-        $('#backTop').removeClass('hide');
-        if(fixed === false) {
-            window.scrollTo(0,50);
-            fixed = true;
-        }
-    }
-    if(window.scrollY == 0) {
+    $('#backTop').click(function(){
         $('#mainNav').removeClass('fixHeader');
-        $('#mainBody').removeClass('fixBody');
-        $('#backTop').addClass('hide');
-        fixed === false;
-    }
+        $('html').animate({scrollTop: 5}, 500);
+    });
+
+    var fixed = false;
+    $(window).scroll(function() {
+        if(window.scrollY > 220) {
+            $('#mainNav').addClass('fixHeader');
+            $('#mainBody').addClass('fixBody');
+            $('#backTop').removeClass('hide');
+            if(fixed === false) {
+                window.scrollTo(0,50);
+                fixed = true;
+            }
+        }
+        if(window.scrollY == 0) {
+            $('#mainNav').removeClass('fixHeader');
+            $('#mainBody').removeClass('fixBody');
+            $('#backTop').addClass('hide');
+            fixed === false;
+        }
+    });
+
 });
